f3b8bfbeff9b25813bf2922836fbc6cc44d886a4
Dirk Rombauts <dirk.rombauts@questmaster.net>
2012-10-24 12:55:43 +0200

    Adding console stuff

--- Cropper/Program.cs
+++ Cropper/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,35 @@ namespace Cropper
   {
     static void Main(string[] args)
     {
+      if (args.Length != 2)
+      {
+        Console.Out.WriteLine("Usage: Cropper <source directory> <target directory>");
+        return;
+      }
+
+      var sourceDirectory = new System.IO.DirectoryInfo(args[0]);
+
+      if (!sourceDirectory.Exists)
+      {
+        Console.Out.WriteLine("Source directory must exist");
+        return;
+      }
+
+      var targetDirectory = new System.IO.DirectoryInfo(args[1]);
+
+      if (!targetDirectory.Exists)
+      {
+        Console.Out.WriteLine("Target directory must exist");
+        return;
+      }
+
+      var croppingThing = new CroppingThing();
+      foreach (var fileInfo in sourceDirectory.EnumerateFiles("*.png", SearchOption.TopDirectoryOnly))
+      {
+        var bitmap = croppingThing.Crop(new Bitmap(fileInfo.FullName));
+
+        bitmap.Save(Path.Combine(targetDirectory.FullName, fileInfo.Name), ImageFormat.Png);
+      }
     }
   }
 }
